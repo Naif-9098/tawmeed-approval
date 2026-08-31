cc70bb355d1a9304287d81f3ed5214c38a5c8d74
Naif-9098 <naif.22220@gmail.com>
2026-08-31 11:33:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
+app.set('trust proxy', 1);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
